557bdb559bdde6b75d391ae9fa10032752d3fece
asheesh859 <ak2305@checkboxtechnology.com>
2025-09-06 15:57:08 +0530

    break the singleton and solve it 6/09/2025 3:56 PM

--- Design_patterns/src/design/Singleton.java
+++ Design_patterns/src/design/Singleton.java
@@ -1,12 +1,14 @@
 package design;
 
-
+import java.lang.reflect.Constructor;
 
 class Samosa{
 	
 	private static Samosa samosa;
 		private Samosa() {
-			
+			if(samosa!=null) {
+				throw new RuntimeException("you are trying to break singleton");
+			}
 		}
 		
 		public static Samosa getSamosa() {
@@ -29,13 +31,20 @@ public class Singleton {
 	
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		Samosa s1 = Samosa.getSamosa();	
 		Samosa s2 = Samosa.getSamosa();
 		
 		System.out.println(s1.hashCode());
 		System.out.println(s2.hashCode());
+		
+		
+		//Break the singleton using reflection
+		Constructor<Samosa> constructor = Samosa.class.getDeclaredConstructor();
+		constructor.setAccessible(true);
+		Samosa s3 = constructor.newInstance();
+		System.out.println(s3.hashCode());
 		
 		
 	}
