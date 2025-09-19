3093677f19ab5b19102fa64dcb990999b4e18d3a
asheesh859 <ak2305@checkboxtechnology.com>
2025-09-19 23:25:42 +0530

    t :wq

--- Design_patterns/src/design/factory.java
+++ Design_patterns/src/design/factory.java
@@ -3,6 +3,5 @@ package design;
 public class factory {
     public static void main(String[] args) {
         System.out.println("Factory pattern example");
-        System.out.println(".")
     }
 }
